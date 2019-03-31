378e22d98513f178bb5aae8b594af79d858de07d
czahie <czahie1996@gmail.com>
2019-03-30 22:10:04 -0400

    3sum

--- 015 3Sum/3sum.py
+++ 015 3Sum/3sum.py
@@ -4,21 +4,22 @@ class Solution:
         nums.sort()
         n = len(nums)
         for i in range(n - 2):
-            if i == 0 or nums[i - 1] != nums[i]:
-                lo = i + 1
-                hi = n - 1
-                total = -nums[i]
-                while (lo < hi):
-                    if nums[lo] + nums[hi] == total:
-                        res.append([nums[i], nums[lo], nums[hi]])
-                        while lo < hi and nums[lo + 1] == nums[lo]:
-                            lo += 1
-                        while lo < hi and nums[hi - 1] == nums[hi]:
-                            hi -= 1
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            lo = i + 1
+            hi = n - 1
+            total = -nums[i]
+            while (lo < hi):
+                if nums[lo] + nums[hi] == total:
+                    res.append([nums[i], nums[lo], nums[hi]])
+                    while lo < hi and nums[lo + 1] == nums[lo]:
                         lo += 1
+                    while lo < hi and nums[hi - 1] == nums[hi]:
                         hi -= 1
-                    elif nums[lo] + nums[hi] > total:
-                        hi -= 1
-                    else:
-                        lo += 1
-        return res
+                    lo += 1
+                    hi -= 1
+                elif nums[lo] + nums[hi] > total:
+                    hi -= 1
+                else:
+                    lo += 1
+        return res 
