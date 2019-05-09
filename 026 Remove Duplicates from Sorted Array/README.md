6916d710f7a71f3c3c014d422844b2a4151d81ec
czahie <czahie1996@gmail.com>
2019-05-09 13:56:33 -0400

    026

---
+++ 026 Remove Duplicates from Sorted Array/README.md
@@ -0,0 +1,2 @@
+# Remove Duplicates from Sorted Array
++ The key here is to set a pointer to remember the number of distinct values. Every time finds a distinct number, put it at this place and increment the number.
---
+++ 026 Remove Duplicates from Sorted Array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            nums[count] = nums[i];
+            count++;
+        }
+        return count;
+    }
+}
