d73393c4f15536fe49d2d7532d97800184424464
czahie <czahie1996@gmail.com>
2019-05-29 23:43:11 +0800

    some array problems

---
+++ 0299 Bulls and Cows/BullsAndCows.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0;
+        int cows = 0;
+        int[] nums = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            int s = secret.charAt(i) - '0';
+            int g = guess.charAt(i) - '0';
+            if (s == g) {
+                bulls++;
+            } else {
+                if (nums[s] < 0) {
+                    cows++;
+                }
+                if (nums[g] > 0) {
+                    cows++;
+                }
+                nums[s]++;
+                nums[g]--;
+            }
+        }
+        return bulls + "A" + cows + "B";
+    }
+}
